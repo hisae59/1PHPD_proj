571e3f2d915df63d05d57ca4fa9c868d7fdf5134
hisae59 <lucie.potier@supinfo.com>
2024-04-10 16:36:38 +0200

    amélioration du style du site, ajout de la page catégories

---
+++ website/src/categories.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categories</title>
+    <LINK rel="stylesheet" type="text/css" href="style/categories.css">
+</head>
+<body>
+    <?php require "basicheader.php";?>
+
+    <div id="categ">
+        <h1><a href="action.php">Action</a></h1>
+        <h1><a href="drama.php">Drama</a></h1>
+    </div>
+</body>
+</html>
--- website/src/index.php
+++ website/src/index.php
@@ -4,13 +4,18 @@
 <head>
     <meta charset="UTF-8"/>
     <title>Homepage</title>
+    <LINK rel="stylesheet" type="text/css" href="style/index.css">
 </head>
 
 <body>
-    <header>
-        <?php require "header.php";?>
-    </header>
-    <h1>Hello World !</h1>
+    <?php require "header.php";?>
+   
+    <h1 id="latestmov"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-camera-reels" viewBox="0 0 16 16">
+  <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0M1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0"/>
+  <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2zm6 8.73V7.27l-3.5 1.555v4.35zM1 8v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1"/>
+  <path d="M9 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6M7 3a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+</svg> <span>Our latest movies:</span></h1>
     <h1>Env user: <?php echo $_ENV['MYSQL_USER'] ?></h1>
+    <?php require "footer.php";?>
 </body>
 </html>
